7c475c90198d61d82b8ff345caba629dda5ad2de
M15z <117819111+M15z@users.noreply.github.com>
2024-02-29 10:56:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+      
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+var typed = new Typed(".auto-type",{
+    strings: ["Web Developer", "Freelancer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
